1dde52db16e48bdffd4784c5cd922d7f863b08a4
suriagadiego <suriagadiego@yahoo.com.ph>
2024-10-31 17:06:21 +0800

    add static file

--- bayc_event/settings.py
+++ bayc_event/settings.py
@@ -115,11 +115,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = "static/"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -130,3 +125,7 @@ CELERY_ENABLED = True
 CELERY_BROKER_URL = "redis://localhost:6379/0"
 
 CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
